a167f3a4eca239ea7ec545dd573f69bd37081925
maxmurjon <maxmurjonkeldiyorov@gmail.com>
2023-03-25 15:45:54 +0500

    Json data

--- lesson11/funtions11/functions11.go
+++ lesson11/funtions11/functions11.go
@@ -1,63 +1,52 @@
 package funtions11
 
 import (
+	"encoding/json"
 	"fmt"
-	"math"
+	"io/ioutil"
 )
 
-type Shape interface {
-	area()
-	perimeter()
+type Methods interface {
+	GetBooks()
+	RemoveBook()
+	UpdateBook()
+	AddBook()
+	GetBooksById()
 }
 
-type Circle struct {
-	radius float64
-}
-
-type Rectangle struct {
-	width  float64
-	height float64
-}
-
-type Uchburchak struct{
-	height float64
-	tomoni1 float64
-	tomoni2 float64
-}
-
-
-func (c Circle) area() float64 {
-	return math.Pi*math.Pow(c.radius,2)
-}
-func (c Circle) perimeter() float64{
-	return 2*math.Pi*c.radius
-}
-
-func (r Rectangle) area() float64{
-	return r.width*r.height
-}
-func (r Rectangle) perimeter() float64{
-	return 2*(r.width+r.height)
-}
-
-func (u Uchburchak) area() float64{
-	return u.height*u.tomoni1/2
-}
-func (u Uchburchak) perimeter() float64{
-	return u.height+u.tomoni1+u.tomoni2
+type Book struct{
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Year     int    `json:"year"`
+	Status   string `json:"status"`
+	Price    int    `json:"price"`
+	Period   int    `json:"period"`
+	Category string `json:"category"`
+	Page     int    `json:"page"`
 }
 
 
-func Return1(){
-	u:=Uchburchak{height:2.0,tomoni1:2,tomoni2:3}
-	c := Circle{radius: 2.5}
-	r := Rectangle{width: 3.0, height: 4.0}
-	
-	fmt.Println(c.area())
-	fmt.Println(r.area())
-	fmt.Println(u.area())
+type Books struct {
+	Books []Book
+}
 
-	fmt.Println(c.perimeter())
-	fmt.Println(r.perimeter())
-	fmt.Println(u.perimeter())
+
+func (data Books) GetBooks()Books{
+	return data
+}
+// func (data *Books) AddBook(title ,author string ,year int,status string,price, period int, category string,page int){
+// 	var newBook map[string]interface{}
+// 	*data=append(*data,newBook)
+// }
+
+func ReadData() {
+	books:=Books{Books:[]Book{}}
+	data, err := ioutil.ReadFile("books.json")
+	if err != nil {
+		panic(err)
+	}
+	err=json.Unmarshal(data, &books)
+	fmt.Println(books)
+
 }
--- lesson11/main.go
+++ lesson11/main.go
@@ -1,7 +1,138 @@
 package main
 
-import "github.com/Golang-bootcamp/lesson11/funtions11"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Methodlari interface {
+	GetBooks() []Kitob
+	AddBook(Kitob) []Kitob
+	RemoveBook(Kitob) []Kitob
+	UpdateBook(Kitob) Kitob
+	GetBookById(Kitob) Kitob
+	// GetBookByCategory(Book)
+}
+
+type Kutubxona struct {
+	Kitoblar []Kitob
+}
+type Kitob struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Year     int    `json:"year"`
+	Status   string `json:"status"`
+	Price    int    `json:"price"`
+	Period   int    `json:"period"`
+	Category string `json:"category"`
+	Page     int    `json:"page"`
+}
+
+func (data Kutubxona) GetBooks() []Kitob {
+	return data.Kitoblar
+}
+
+func (data Kutubxona) GetBooksById(id int) Kitob {
+	var kitob1 Kitob
+	for _, v := range data.Kitoblar {
+		if v.ID == id {
+			kitob1.ID = v.ID
+			kitob1.Title = v.Title
+			kitob1.Author = v.Author
+			kitob1.Year = v.Year
+			kitob1.Status = v.Status
+			kitob1.Price = v.Price
+			kitob1.Period = v.Period
+			kitob1.Category = v.Category
+			kitob1.Page = v.Page
+			return kitob1
+		}
+	}
+	return kitob1
+}
+
+func (data *Kutubxona) AddBook(kitob Kitob) []Kitob {
+	data.Kitoblar = append(data.Kitoblar, kitob)
+	kitoblar := data.Kitoblar
+
+	jsonData, _ := json.Marshal(kitoblar)
+
+	ioutil.WriteFile("books.json", jsonData, 0644)
+	return data.Kitoblar
+}
+
+func (data *Kutubxona) RemoveBook(kitob Kitob) []Kitob{
+	for i, v := range data.Kitoblar {
+		if v.ID == kitob.ID {
+			data.Kitoblar = append(data.Kitoblar[:i], data.Kitoblar[i+1:]...)
+			break
+		}
+	}
+
+	kitoblar := data.Kitoblar
+	jsonData, _ := json.Marshal(kitoblar)
+	ioutil.WriteFile("books.json", jsonData, 0644)
+
+	return data.Kitoblar
+}
+
+func (data Kutubxona) UpdateBook(kitob Kitob) Kitob {
+	for i, _ := range data.Kitoblar {
+		if data.Kitoblar[i].ID == kitob.ID {
+			data.Kitoblar[i].ID=kitob.ID 
+			data.Kitoblar[i].Title=kitob.Title 
+			data.Kitoblar[i].Author=kitob.Author
+			data.Kitoblar[i].Year=kitob.Year 
+			data.Kitoblar[i].Status=kitob.Status  
+			data.Kitoblar[i].Price=kitob.Price
+			data.Kitoblar[i].Period=kitob.Period 
+			data.Kitoblar[i].Category=kitob.Category 
+			data.Kitoblar[i].Page=kitob.Page 
+			kitoblar := data.Kitoblar
+			jsonData, _ := json.Marshal(kitoblar)
+			ioutil.WriteFile("books.json", jsonData, 0644)
+			return data.Kitoblar[i]
+		}
+	}
+	return data.Kitoblar[kitob.ID]
+}
+
 
 func main() {
-	funtions11.Return1()
+	var kitoblar []Kitob
+	data, err := ioutil.ReadFile("books.json")
+	if err != nil {
+		panic("Json datani o'qishda hato")
+	}
+	err = json.Unmarshal(data, &kitoblar)
+	if err != nil {
+		panic("Unmarshal qilishda muammo")
+	}
+
+	kutubxona := Kutubxona{kitoblar}
+
+	newBook := Kitob{
+		ID:     1,
+		Title:  "Jaloladdin",
+		Author: "hadfioh",
+		Year:   2004,
+		Status: "given",
+		Price:  890,
+	}
+
+	// fmt.Println(kutubxona.GetBooks())
+	// fmt.Println(kutubxona.RemoveBook(newBook))
+	// fmt.Println(kutubxona.GetBooksById(2))
+	fmt.Println(kutubxona.UpdateBook(newBook))
+
+}
+ok := Kitob{
+	ID:     1,
+	Title:  "Jaloladdin",
+	Author: "hadfioh",
+	Year:   2004,
+	Status: "given",
+	Price:  890,
 }
